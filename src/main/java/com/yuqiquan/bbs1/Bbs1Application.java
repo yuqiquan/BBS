dfa518808db5d01007c8083cba50f7128be3e710
yuqiquan <3297269491@qq.com>
2021-05-08 16:40:23 +0800

    init repo

---
+++ src/main/java/com/yuqiquan/bbs1/Bbs1Application.java
@@ -0,0 +1,13 @@
+package com.yuqiquan.bbs1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bbs1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bbs1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuqiquan/bbs1/Bbs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuqiquan.bbs1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bbs1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
